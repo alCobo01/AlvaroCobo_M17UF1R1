5298ffda5ada0d119de147d39a9fbb62b238793e
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-11-11 16:52:54 +0200

    bruh

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,19 +1,26 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float startpos;
+    private float _startpos, _lenght;
     [SerializeField] private GameObject gameCamera;
     [SerializeField] private float parallaxEffect;
 
     void Start()
     {
-        startpos = transform.position.x;
+        _startpos = transform.position.x;
+        _lenght = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    void FixedUpdate()
+    void LateUpdate()
     {
         float distance = gameCamera.transform.position.x * parallaxEffect;
-        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+        float temp = gameCamera.transform.position.x * (1 - parallaxEffect);
+
+        if (temp > _startpos + _lenght) _startpos += _lenght;
+        else if (temp < _startpos - _lenght) _startpos -= _lenght;
+
+        transform.position = new Vector3(_startpos + distance, transform.position.y, transform.position.z);
     }
 }
